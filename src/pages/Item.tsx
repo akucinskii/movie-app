5c063a0500d78c20df3304d3d94ef34f5e47d210
Artur Kucinski <artuyees@gmail.com>
2022-02-15 21:29:40 +0100

    added item page logic

--- src/pages/Item.tsx
+++ src/pages/Item.tsx
@@ -3,21 +3,40 @@ import { useParams } from "react-router-dom";
 
 export interface IItemProps {}
 
+interface Item {
+  Title?: string;
+  Actors?: string;
+  Poster?: string;
+  Response?: boolean;
+}
+
 const Item: React.FunctionComponent<IItemProps> = (props) => {
-  const [message, setMessage] = useState("");
+  const [data, setData] = useState<Item>({});
   const { number } = useParams();
+
+  const fetchItems = async (req: any) => {
+    const data: any = await fetch(
+      `http://www.omdbapi.com/?apikey=3458304a&t=${req}`
+    );
+    const items: any = await data.json();
+    console.log(items);
+    if (items.Response === "False") {
+      alert("No movie found");
+    } else {
+      setData(items);
+    }
+  };
 
   useEffect(() => {
     if (number) {
-      setMessage("the number is" + number);
-    } else {
-      setMessage("error");
+      fetchItems(number);
     }
-  }, []);
+  }, [number]);
   return (
     <div>
       <p>Item page</p>
-      <p>{message}</p>
+      <p>{data.Title}</p>
+      <img src={data.Poster} alt="" />
     </div>
   );
 };
