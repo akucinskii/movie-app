d64fd54b765193624962a381ec80ac868b66f9ca
Artur Kucinski <artuyees@gmail.com>
2022-02-17 20:14:04 +0100

    finished up

--- src/pages/Popular.tsx
+++ src/pages/Popular.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Cards from "../components/organisms/Cards";
+import { movies } from "../data/MostPopular";
 
 export interface IPopularProps {}
 
 const Popular: React.FunctionComponent<IPopularProps> = (props) => {
   return (
-    <div>
-      <p></p>
+    <div className="flex justify-center h-full">
+      <div className="mt-20">
+        <h1 className="text-4xl text-white text-center italic mb-2">
+          Most popular:
+        </h1>
+        <Cards movies={movies} />
+      </div>
     </div>
   );
 };
