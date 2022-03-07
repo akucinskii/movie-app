9f145f2fc268dfb47ecd38e3431689e704ce7a15
Artur Kuciński <artuyees@gmail.com>
2022-03-07 12:24:52 +0100

    fix

--- src/pages/Item.tsx
+++ src/pages/Item.tsx
@@ -19,9 +19,10 @@ interface Items {
 const Item: React.FunctionComponent<IItemProps> = (props) => {
   const navigate = useNavigate();
   const [data, setData] = useState<Items>({});
-  const { res } = useParams();
+  const { number } = useParams();
 
   const fetchItems = async (req: string) => {
+    console.log("fetch")
     const data: any = await fetch(
       `https://www.omdbapi.com/?apikey=3458304a&t=${req}`
     );
@@ -36,11 +37,11 @@ const Item: React.FunctionComponent<IItemProps> = (props) => {
   };
 
   useEffect(() => {
-    if (res) {
-      fetchItems(res);
+    if (number) {
+      fetchItems(number);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [res]);
+  }, [number]);
   return (
     <div className="flex w-full h-full justify-center">
         <div className="flex flex-col md:flex-row max-w-screen md:max-w-[700px] my-16 md:my-24 m-2 p-1 bg-[#232323] rounded-lg text-white">
